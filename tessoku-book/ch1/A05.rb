b30b3eacf4a11c3e7ead66f88ae4d36f2b21617a
djkazunoko <djkazunoko@gmail.com>
2025-08-30 18:38:01 +0900

    A05提出(AC) (#6)
    解説を見た後に実装したもの

--- tessoku-book/ch1/A05.rb
+++ tessoku-book/ch1/A05.rb
@@ -1,14 +1,11 @@
 n,k = gets.chomp.split.map(&:to_i);
 count = 0
-
 (1..n).each do |a|
   (1..n).each do |b|
-    (1..n).each do |c|
-      if (a + b + c) == k
-        count += 1
-      end
+    c = k - a - b
+    if c >= 1 && c <= n
+      count += 1
     end
   end
 end
-
 puts count
