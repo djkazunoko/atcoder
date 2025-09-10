9ed5780fc51ce27b08f6148821d59107fc90a192
djkazunoko <djkazunoko@gmail.com>
2025-09-10 15:17:56 +0900

    ABS-ABC085C 再提出(AC) (#12)

--- ABS/ABC085C.rb
+++ ABS/ABC085C.rb
@@ -5,11 +5,10 @@ max = N * 10000
 res = [N, 0, 0] if max == Y
 
 if max > Y
-  x = Y / 10000
-  yz = N - x
-  (0..yz).each do |y|
-    (0..(yz - y)).each do |z|
-      if (5000 * y) + (1000 * z) == (Y % 10000)
+  (0..N).each do |x|
+    (0..(N - x)).each do |y|
+      z = N - x - y
+      if (10000 * x) + (5000 * y) + (1000 * z) == Y
         res = [x, y, z]
       end
     end
