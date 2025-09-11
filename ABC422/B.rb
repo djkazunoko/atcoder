47b1589c90534e84bc6b12241c4930e03bf272bc
djkazunoko <djkazunoko@gmail.com>
2025-09-11 11:50:29 +0900

    ABC422-B 再提出(AC) (#13)
    解説を見た後に実装

--- ABC422/B.rb
+++ ABC422/B.rb
@@ -5,25 +5,27 @@ res = 'Yes'
 
 H.times do |h|
   W.times do |w|
+    next if arr_arr[h][w] == '.'
+
     count = 0 
-    if arr_arr[h][w] == '#'
-      if (w + 1) != W && arr_arr[h][w + 1] == '#'
-        count += 1
-      end
 
-      if (w - 1) >= 0 && arr_arr[h][w - 1] == '#'
-        count += 1
-      end
-
-      if (h + 1) != H && arr_arr[h + 1][w] == '#'
-        count += 1
-      end
-
-      if (h - 1) >= 0 && arr_arr[h - 1][w] == '#'
-        count += 1
-      end
+    if (w + 1) != W && arr_arr[h][w + 1] == '#'
+      count += 1
     end
-    if count == 1 || count == 3
+
+    if (w - 1) >= 0 && arr_arr[h][w - 1] == '#'
+      count += 1
+    end
+
+    if (h + 1) != H && arr_arr[h + 1][w] == '#'
+      count += 1
+    end
+
+    if (h - 1) >= 0 && arr_arr[h - 1][w] == '#'
+      count += 1
+    end
+
+    unless count == 2 || count == 4
       res = 'No'
     end
   end
