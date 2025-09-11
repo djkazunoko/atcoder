742f84cb218626d21053311989ab5a83e5e64b59
djkazunoko <djkazunoko@gmail.com>
2025-09-11 11:54:59 +0900

    ABC422-B リファクタリングして再提出(AC) (#13)

--- ABC422/B.rb
+++ ABC422/B.rb
@@ -1,29 +1,17 @@
 H,W = gets.chomp.split.map(&:to_i)
-arr_arr = $<.map{_1.chomp.split("")}
+board = $<.map{_1.chomp.chars}
 
 res = 'Yes'
 
 H.times do |h|
   W.times do |w|
-    next if arr_arr[h][w] == '.'
+    next if board[h][w] == '.'
 
     count = 0 
-
-    if (w + 1) != W && arr_arr[h][w + 1] == '#'
-      count += 1
-    end
-
-    if (w - 1) >= 0 && arr_arr[h][w - 1] == '#'
-      count += 1
-    end
-
-    if (h + 1) != H && arr_arr[h + 1][w] == '#'
-      count += 1
-    end
-
-    if (h - 1) >= 0 && arr_arr[h - 1][w] == '#'
-      count += 1
-    end
+    count += 1 if w + 1 < W && board[h][w + 1] == '#'
+    count += 1 if w - 1 >= 0 && board[h][w - 1] == '#'
+    count += 1 if h + 1 < H && board[h + 1][w] == '#'
+    count += 1 if h - 1 >= 0 && board[h - 1][w] == '#'
 
     unless count == 2 || count == 4
       res = 'No'
