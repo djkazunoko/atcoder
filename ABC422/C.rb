36ad95dfc762b8d428bbf173913a4df5d56ff3fb
djkazunoko <djkazunoko@gmail.com>
2025-09-12 12:55:19 +0900

    ABC422-C 再提出(AC) (#13)
    解説を見てから実装

--- ABC422/C.rb
+++ ABC422/C.rb
@@ -1,22 +1,5 @@
-T = gets.to_i
-ABC = $<.map{ _1.split.map(&:to_i) }
-
-res = []
-ABC.each do |abc|
-  ac = []
-  b = abc[1]
-  ac << abc[0]
-  ac << abc[2]
-
-  amari = (b + ac.max) - ac.min
-
-  if ac.min <= amari
-    res << ac.min
-    next
-  end
-
-  res << amari + ((ac.min - amari) / 2 + (ac.min - amari) % 2)
-
+t = gets.to_i
+t.times do
+  a, b, c = gets.split.map(&:to_i)
+  puts [a, c, (a + b + c) / 3].min
 end
-
-puts res
