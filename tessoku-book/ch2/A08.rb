b29a25f85efc9dfb2602c06155a1ec27a83c6b2a
djkazunoko <djkazunoko@gmail.com>
2025-09-15 15:02:12 +0900

    tessoku-book ch2 A08 リファクタリングして再提出(AC) (#11)
    最終的に二次元累積和のマス目しか使わないので、元のマス目と1次元累積和のマス目を残す処理を削除

--- tessoku-book/ch2/A08.rb
+++ tessoku-book/ch2/A08.rb
@@ -9,41 +9,30 @@ Q.times do
   points << gets.split.map(&:to_i)
 end
 
-board_1d = []
-
 board.each do |row|
-  tmp = []
   row.each_with_index do |n, idx|
     if idx > 0
-      tmp << (n + tmp[idx - 1])
-    else
-      tmp << n
+      row[idx] = (n + row[idx - 1])
     end
   end
-  board_1d << tmp
 end
 
-board_2d = []
-board_2d << board_1d[0]
-
 (1..(H - 1)).each do |h|
-  tmp = []
   W.times do |w|
-    tmp << (board_1d[h][w] + board_2d[h-1][w])
+    board[h][w] += board[h-1][w]
   end
-  board_2d << tmp
 end
 
 res = []
-board_2d.unshift Array.new(W, 0)
+board.unshift Array.new(W, 0)
 (H+1).times do |h|
-  board_2d[h].unshift 0
+  board[h].unshift 0
 end
 
 points.each do |abcd|
   a,b,c,d = abcd
-  bottom_right = board_2d[c][d]
-  res << bottom_right - (board_2d[c][b-1] + board_2d[a-1][d]) + board_2d[a-1][b-1]
+  bottom_right = board[c][d]
+  res << bottom_right - (board[c][b-1] + board[a-1][d]) + board[a-1][b-1]
 end
 
 puts res
