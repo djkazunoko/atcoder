6b05b967f3a9b17613e37c03e7885c798681f28e
djkazunoko <djkazunoko@gmail.com>
2025-09-16 21:36:50 +0900

    ABC423-C 解説を読んで修正して再提出(AC) (#14)

--- ABC423/C.rb
+++ ABC423/C.rb
@@ -1,23 +1,21 @@
 N, R = gets.split.map(&:to_i)
 l_ary = gets.split.map(&:to_i)
 
-right = l_ary.slice!(R..)
-left = l_ary
+right = l_ary[R..]
+left = l_ary[0...R]
 
-right.reverse.each_with_index do |n, idx|
-  if n == 0
-    right.pop(idx)
-    break
-  end
+if (i = right.rindex(0))
+  right = right[0..i]
+else
+  right = []
 end
-r_cnt = right.count(1) + right.size
+r_cnt = right.count(0) + right.count(1) * 2
 
-left.each_with_index do |n, idx|
-  if n == 0
-    left.shift(idx)
-    break
-  end
+if (i = left.index(0))
+  left = left[i..]
+else
+  left = []
 end
-l_cnt = left.count(1) + left.size
+l_cnt = left.count(0) + left.count(1) * 2
 
 p r_cnt + l_cnt
