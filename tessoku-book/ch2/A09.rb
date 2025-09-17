6baf0e142388f1981ad550522ae955993920687b
djkazunoko <djkazunoko@gmail.com>
2025-09-17 13:53:34 +0900

    tessoku-book ch2 A09 修正して再提出(AC) (#11)
    番兵を追加

--- tessoku-book/ch2/A09.rb
+++ tessoku-book/ch2/A09.rb
@@ -3,28 +3,31 @@ points_ary_ary = []
 N.times do
   points_ary_ary << gets.split.map(&:to_i)
 end
-board = Array.new(H) {Array.new(W, 0)}
+board = Array.new(H+2) {Array.new(W+2, 0)}
 
 points_ary_ary.each do |points_ary|
-  a,b,c,d = points_ary.map{_1 - 1}
+  a,b,c,d = points_ary
   board[a][b] += 1
   board[c+1][d+1] += 1
   board[a][d+1] -= 1
   board[c+1][b] -= 1
 end
 
-board.each do |row|
-  row.each_with_index do |n, idx|
-    if idx > 0
-      row[idx] = (n + row[idx - 1])
-    end
+(1..H).each do |i|
+  (1..W).each do |j|
+    board[i][j] += board[i][j-1]
   end
 end
 
-(1..(H - 1)).each do |h|
-  W.times do |w|
-    board[h][w] += board[h-1][w]
+(1..H).each do |i|
+  (1..W).each do |j|
+    board[i][j] += board[i-1][j]
   end
 end
+
+board.shift
+board.pop
+board.map{_1.shift}
+board.map{_1.pop}
 
 puts board.map{_1.join(' ')}
