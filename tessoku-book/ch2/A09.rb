2cb17f3ba18f2d9844fea3f82b18c2835afbb9ff
djkazunoko <djkazunoko@gmail.com>
2025-09-17 13:31:57 +0900

    tessoku-book ch2 A09 解説を見て修正して再提出(RE) (#11)

--- tessoku-book/ch2/A09.rb
+++ tessoku-book/ch2/A09.rb
@@ -7,8 +7,23 @@ board = Array.new(H) {Array.new(W, 0)}
 
 points_ary_ary.each do |points_ary|
   a,b,c,d = points_ary.map{_1 - 1}
-  (a..c).each do |i|
-    board[i][b..d] = board[i][b..d].map{_1 + 1}
+  board[a][b] += 1
+  board[c+1][d+1] += 1
+  board[a][d+1] -= 1
+  board[c+1][b] -= 1
+end
+
+board.each do |row|
+  row.each_with_index do |n, idx|
+    if idx > 0
+      row[idx] = (n + row[idx - 1])
+    end
+  end
+end
+
+(1..(H - 1)).each do |h|
+  W.times do |w|
+    board[h][w] += board[h-1][w]
   end
 end
 
