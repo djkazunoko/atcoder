613a3768ac89e4fd9a48dc3b5b510a9806ce5907
djkazunoko <djkazunoko@gmail.com>
2025-09-30 11:19:46 +0900

    tessoku-book ch2 A10 解説を見て実装して再提出(TLE) (#11)

--- tessoku-book/ch2/A10.rb
+++ tessoku-book/ch2/A10.rb
@@ -6,11 +6,30 @@ d.times do
   lr_ary_ary << gets.split.map(&:to_i)
 end
 
-res = []
-
-lr_ary_ary.each do |lr_ary|
-  l,r = lr_ary
-  res << a_ary[..(l - 2)].concat(a_ary[r..]).max
+# 左から累積max
+p = [a_ary[0]]
+1.upto(a_ary.length - 1) do |i|
+  if a_ary[i] > p[i-1]
+    p << a_ary[i]
+  else
+    p << p[i-1]
+  end
 end
 
+# 右から累積max
+q = Array.new(n, 0)
+q[-1] = a_ary[-1]
+(n-2).downto(0) do |i|
+  if a_ary[i] > q[i+1]
+    q[i] = a_ary[i]
+  else
+    q[i] = q[i+1]
+  end
+end
+
+res = []
+lr_ary_ary.each do |lr_ary|
+  l,r = lr_ary
+  res << p[..(l-2)].concat(q[r..]).max
+end
 puts res
