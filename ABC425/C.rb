6689b02cd600c5564e8307a146673d22f701fe61
djkazunoko <djkazunoko@gmail.com>
2025-10-07 10:36:44 +0900

    ABC425-C 解説を参考に修正して再提出(AC) (#20)

--- ABC425/C.rb
+++ ABC425/C.rb
@@ -1,19 +1,26 @@
 n,q = gets.split.map(&:to_i)
-a_ary = gets.split.map(&:to_i)
+a = gets.split.map(&:to_i)
 query_ary = []
 q.times do
   query_ary << gets.split.map(&:to_i)
 end
-b_ary = a_ary.concat a_ary
+
+b = a + a
+
+s = Array.new(2*n + 1, 0)
+(1..2*n).each do |i|
+  s[i] = s[i-1] + b[i-1]
+end
+
 x = 0
+
 query_ary.each do |query|
-  if query.length == 2
-    c = query.last
+  if query[0] == 1
+    c = query[1]
     x += c
+    x %= n
   else
-    l = query[1]
-    r = query[2]
-    y = x % n
-    puts b_ary[(l - 1 + y)..(r - 1 + y)].sum
+    l, r = query[1], query[2]
+    puts s[r + x] - s[l - 1 + x]
   end
 end
