22118bc8c13dfb1a7b1bf4f5e5a653e70c8f796c
djkazunoko <djkazunoko@gmail.com>
2025-10-26 11:46:46 +0900

    ABC429-B 解説を参考に実装して再提出(AC) (#24)

--- ABC/429/B.rb
+++ ABC/429/B.rb
@@ -1,10 +1,9 @@
 n,m = gets.split.map(&:to_i)
 a = gets.split.map(&:to_i)
 
-a.concat a
 res = 'No'
-n.times do |i|
-  res = 'Yes' if a.slice(i, (n-1)).sum == m
+a.each do |ak|
+   res = 'Yes' if ak == a.sum - m
 end
 
 puts res
