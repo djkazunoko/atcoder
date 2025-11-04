9a9eae1bf5d914c2417e52c66cec0c2b2df5cb58
djkazunoko <djkazunoko@gmail.com>
2025-11-04 18:26:45 +0900

    ABC430-C 提出(WA) (#25)

---
+++ ABC/430/C.rb
@@ -0,0 +1,18 @@
+n,a,b = gets.split.map(&:to_i)
+s = gets.chomp
+
+res = []
+
+(0..(n-a+1)).each do |l|
+  (l..(n-1)).each do |r|
+    if s[l..r].count('b') >= b
+      break
+    end
+    if s[l..r].count('a') >= a
+      res.push([(l+1), (r+1)])
+    end
+  end
+end
+
+p res
+
