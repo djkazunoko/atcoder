007995f918698bb45edeca8fbfc9a1be9529e5d0
djkazunoko <djkazunoko@gmail.com>
2025-11-18 16:11:53 +0900

    tessoku-book ch3 A13 解法2(しゃくとり法)で実装して提出(AC) (#18)

--- tessoku-book/ch3/A13.rb
+++ tessoku-book/ch3/A13.rb
@@ -1,11 +1,18 @@
 n,k = gets.split.map(&:to_i)
 a = gets.split.map(&:to_i)
 
+r = []
+
+(n-1).times do |i|
+  r[i] = (i == 0) ? 1 : r[i-1]
+  while r[i] < n && (a[r[i]] - a[i] <= k) do
+    r[i] += 1
+  end
+end
+
 ans = 0
 (n-1).times do |i|
-  idx = a.bsearch_index { _1 > (a[i] + k) }
-  r = idx ? idx : n
-  ans += r - (i+1)
+  ans += (r[i] - (i + 1))
 end
 
 puts ans
