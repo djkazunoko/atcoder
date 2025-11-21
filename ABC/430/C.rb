7c722553846b25a8e20706932e309944c1be5085
djkazunoko <djkazunoko@gmail.com>
2025-11-21 14:37:26 +0900

    ABC430-C 公式解説の解法(二分探索)で実装して再提出(AC) (#25)

--- ABC/430/C.rb
+++ ABC/430/C.rb
@@ -1,29 +1,39 @@
 n,a,b = gets.split.map(&:to_i)
 s = gets.chomp
 
-res = []
+# 各区間に含まれる'a' , 'b' の個数の累積和
+xa = [0]
+xb = [0]
 
-l = 0
-r = b - 1
-
-loop do
-  if s[l..r].count('b') >= b
-    l += 1
-    r = l + (b - 1)
-  end
-  if s[l..r].count('a') >= a
-    res.push([(l+1), (r+1)])
-  end
-  if r < (n - 1)
-    r += 1 
-  else
-    l += 1
-    r = l + (b - 1)
-  end
-  if l > (n-a+1)
-    break
-  end
+n.times do |i|
+  xa[i+1] = s[i] == 'a' ? xa[i] + 1 : xa[i]
+  xb[i+1] = s[i] == 'b' ? xb[i] + 1 : xb[i]
 end
 
-puts res.length
+# 「[l,r] に含まれる 'a' の個数が a 個以上となる最小の r」 の配列
+a_l_ary = []
+(1..n).each do |l|
+  a_l = (l..n).bsearch do |r|
+    xa[r] - xa[l-1] >= a
+  end
+  a_l_ary.push a_l
+end
+
+# 「[l,r] に含まれる 'b' の個数が b 個未満となる最大の r」 の配列
+b_l_ary = []
+(1..n).each do |l|
+  first_bad = (l..n).bsearch do |r|
+    xb[r] - xb[l-1] >= b
+  end
+  b_l = first_bad.nil? ? n : (first_bad - 1)
+  b_l_ary.push b_l
+end
+
+# 回答
+ans = 0
+n.times do |i|
+  next if a_l_ary[i].nil?
+  ans += [0, (b_l_ary[i] - a_l_ary[i] + 1)].max
+end
+puts ans
 
