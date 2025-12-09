cb75440b4a7666347ecd6da4ec5280875881afad
djkazunoko <djkazunoko@gmail.com>
2025-12-09 22:56:22 +0900

    ABC430-C 公式解説の解法(しゃくとり法)で実装して再提出(AC) (#25)

--- ABC/430/C.rb
+++ ABC/430/C.rb
@@ -10,30 +10,22 @@ n.times do |i|
   xb[i+1] = s[i] == 'b' ? xb[i] + 1 : xb[i]
 end
 
-# 「[l,r] に含まれる 'a' の個数が a 個以上となる最小の r」 の配列
-a_l_ary = []
-(1..n).each do |l|
-  a_l = (l..n).bsearch do |r|
-    xa[r] - xa[l-1] >= a
-  end
-  a_l_ary.push a_l
-end
-
-# 「[l,r] に含まれる 'b' の個数が b 個未満となる最大の r」 の配列
-b_l_ary = []
-(1..n).each do |l|
-  first_bad = (l..n).bsearch do |r|
-    xb[r] - xb[l-1] >= b
-  end
-  b_l = first_bad.nil? ? n : (first_bad - 1)
-  b_l_ary.push b_l
-end
-
-# 回答
+rA = 1
+rB = 1
 ans = 0
-n.times do |i|
-  next if a_l_ary[i].nil?
-  ans += [0, (b_l_ary[i] - a_l_ary[i] + 1)].max
-end
-puts ans
 
+(1..n).each do |l|
+  while rA <= n && xa[rA] - xa[l-1] < a
+    rA += 1
+  end
+
+  break if rA > n
+
+  while rB <= n && xb[rB] - xb[l-1] < b
+    rB += 1
+  end
+
+  ans += [0, rB - rA].max
+end
+
+puts ans
