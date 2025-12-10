1aa0309e2f8ea9954df7a7079e78e33d9ef86647
djkazunoko <djkazunoko@gmail.com>
2025-12-10 15:22:54 +0900

    ABC431-B 公式解説のより高速な解法で実装して再提出(AC) (#26)

--- ABC/431/B.rb
+++ ABC/431/B.rb
@@ -4,18 +4,17 @@ w_ary = gets.split.map(&:to_i)
 q = gets.to_i
 p_ary = q.times.map { gets.to_i }
 
-parts = []
+b = Array.new(n, false)
+weight = x
 
 p_ary.each do |p|
-  if parts.include?(p)
-    parts.delete(p)
+  p -= 1
+  if b[p]
+    b[p] = false
+    weight -= w_ary[p]
   else
-    parts.push p
+    b[p] = true
+    weight += w_ary[p]
   end
-  
-  res = x
-  parts.each do |part|
-    res += w_ary[part - 1]
-  end
-  puts res
+  puts weight
 end
