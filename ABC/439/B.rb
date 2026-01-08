a8c214c9a36f27773ebc59ceb28fab24d8c126db
djkazunoko <djkazunoko@gmail.com>
2026-01-08 18:32:04 +0900

    ABC439-B 解説の解法で実装して再提出(AC) (#34)

--- ABC/439/B.rb
+++ ABC/439/B.rb
@@ -1,20 +1,18 @@
 n = gets.to_i
 
-res = 'No'
-a = []
-
-loop do
-  if n == 1
-    res = 'Yes'
-    break
-  end
-
-  a << n
-  if a.size != a.uniq.size
-    break
-  end
-
-  n = n.to_s.chars.map(&:to_i).map { _1 ** 2 }.sum
+def f(n)
+  n.to_s.chars.map { _1.to_i ** 2 }.sum
 end
 
-puts res
+history = Set.new
+
+while n > 1
+  if history.include?(n)
+    puts 'No'
+    exit
+  end
+  history.add(n)
+  n = f(n)
+end
+
+puts 'Yes'
