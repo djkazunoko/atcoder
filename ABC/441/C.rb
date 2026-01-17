068f62232dcb2a0b3a2d269cab79df93bfca21b0
djkazunoko <djkazunoko@gmail.com>
2026-01-17 22:28:11 +0900

    ABC441-C (WA) (#37)

--- ABC/441/C.rb
+++ ABC/441/C.rb
@@ -1,23 +1,26 @@
 n,k,x = gets.split.map(&:to_i)
 a_ary = gets.split.map(&:to_i)
 
-# 量が少ないコップを酒とする
+# 量が少ないコップから順に酒が入っていたとする
 sake_ary = a_ary.sort[0...k]
+water_ary = a_ary.sort[k..]
 
-t_sake = 0
+# 水が入っている = 酒が0ml
+sake_zero_ary = Array.new(water_ary.size, 0)
+
+sake_ary.concat sake_zero_ary
+
+t_sake_ml = 0
 cup = 0
-sake_ary.each_with_index do |sake, idx|
-  t_sake += sake
+sake_ary.sort.each do |sake_ml|
+  t_sake_ml += sake_ml
   cup += 1
-  if t_sake > x
+  if t_sake_ml >= x
     break
   end
 end
 
-cup += a_ary.sort[k..].size
-
-if t_sake < x
+if t_sake_ml < x
   cup = -1
 end
-
 puts cup
