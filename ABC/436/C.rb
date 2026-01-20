7289f5d7147c4bf7c7c52fdf8b84dda8f13a40cf
djkazunoko <djkazunoko@gmail.com>
2026-01-20 12:28:56 +0900

    ABC436-C 修正(AC) (#31)

--- ABC/436/C.rb
+++ ABC/436/C.rb
@@ -2,14 +2,22 @@ n,m = gets.split.map(&:to_i)
 rc_arr = m.times.map { gets.split.map(&:to_i) }
 
 res = 0
-occupied_cells = Set.new
+st = Set.new
 
 rc_arr.each do |rc|
   r,c = rc
-  rc_block = [[r, c], [(r + 1), c], [r, (c + 1)], [(r + 1), (c + 1)]]
 
-  if occupied_cells.disjoint?(rc_block)
-    rc_block.map { occupied_cells << _1 }
+  ok = true
+  ((r - 1)..(r + 1)).each do |x|
+    ((c - 1)..(c + 1)).each do |y|
+      if st.include?([x, y])
+        ok = false
+      end
+    end
+  end
+
+  if ok
+    st << [r, c]
     res += 1
   end
 end
