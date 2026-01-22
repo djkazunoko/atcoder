593d89f05de1d79aeab7187de47789041af41eb3
djkazunoko <djkazunoko@gmail.com>
2026-01-22 18:12:04 +0900

    ABC438-C 解説を見て修正(AC) (#33)

--- ABC/438/C.rb
+++ ABC/438/C.rb
@@ -1,41 +1,12 @@
 n = gets.to_i
-a = gets.split.map(&:to_i)
+a_ary = gets.split.map(&:to_i)
 
-# 4文字未満なら終了
-if a.size < 4
-  puts a.size
-  exit
-end
-
-# 最初の4連の左端と右端のインデックスを取得
-l = 0
-r = 1
-start = []
-while r < n
-  if a[l] == a[r]
-    if (r - l) == 3
-      start << l
-      start << r
-      break
-    end
-    r += 1
-  else
-    l = r
+q = []
+a_ary.each do |a|
+  q << a
+  if q.size >= 4 && (q[-1] == q[-2] && q[-2] == q[-3] && q[-3] == q[-4])
+    q.slice!(-4..-1)
   end
 end
 
-# 4連がなければ終了
-if start.empty?
-  puts a.size
-  exit
-end
-
-puyopuyo = 4
-left, right = start
-left -= 1
-right += 1
-# while left > 0 || right < a.size
-  # 4連の左端より左には最大で3連までしか存在しない(4連の右端より右には4連以上ある可能性あり)
-# end
-
-p a.size - puyopuyo
+p q.size
