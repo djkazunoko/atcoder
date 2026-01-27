79a3b7bff0049f45a2fef9642014b2bd4177ec5d
djkazunoko <djkazunoko@gmail.com>
2026-01-27 23:07:30 +0900

    AOJ ITP1_9_D (WA) (#45)

---
+++ AOJ/ITP1/9_D.rb
@@ -0,0 +1,18 @@
+str = gets.chomp
+q = gets.to_i
+inst_ary = q.times.map { gets.split }
+
+inst_ary.each do |inst|
+  l = inst[1].to_i
+  r = inst[2].to_i
+
+  if inst[0] == "print"
+    puts str[l..r]
+  elsif inst[0] == "reverse"
+    rev_str = str[l..r].reverse
+    str.slice!(l..r)
+    str.prepend(rev_str)
+  elsif inst[0] == "replace"
+    str[l..r] = inst[3]
+  end
+end
