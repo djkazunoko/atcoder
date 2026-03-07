212f341407f0557963f9e232f837c43db2c8bead
djkazunoko <djkazunoko@gmail.com>
2026-03-07 11:46:41 +0900

    sumitrust2019_d (TLE) (#47)

--- brute-force/sumitrust2019_d.rb
+++ brute-force/sumitrust2019_d.rb
@@ -1,13 +1,17 @@
 n = gets.to_i
 s = gets.chomp.chars
 
-pins = []
-n.times do |i|
-  ((i + 1)..(n - 1)).each do |j|
-    ((j + 1)..(n - 1)).each do |k|
-      pins << s[i] + s[j] + s[k]
+ans = 0
+two_chars = []
+(0..(n - 3)).each do |i|
+  ((i + 1)..(n - 2)).each do |j|
+    two_char = s[i] + s[j]
+    unless two_chars.include?(two_char)
+      two_chars << s[i] + s[j]
+      third_chars = s[(j + 1)..(-1)]
+      ans += third_chars.uniq.size
     end
   end
 end
 
-puts pins.uniq.size
+puts ans
