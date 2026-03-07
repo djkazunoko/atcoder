60fff157a9e3c33b04ff0a0bdb5da5ec63005e27
djkazunoko <djkazunoko@gmail.com>
2026-03-07 11:12:29 +0900

    sumitrust2019_d (TLE) (#47)

--- brute-force/sumitrust2019_d.rb
+++ brute-force/sumitrust2019_d.rb
@@ -1,4 +1,13 @@
 n = gets.to_i
 s = gets.chomp.chars
 
-puts s.combination(3).to_a.uniq.size
+pins = []
+n.times do |i|
+  ((i + 1)..(n - 1)).each do |j|
+    ((j + 1)..(n - 1)).each do |k|
+      pins << s[i] + s[j] + s[k]
+    end
+  end
+end
+
+puts pins.uniq.size
