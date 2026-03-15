80459853c8be13a1b845081ae280afb5bebe9d40
djkazunoko <djkazunoko@gmail.com>
2026-03-15 14:06:19 +0900

    ABC449-C 解説を見て実装(AC) (#56)

--- ABC/449/C.rb
+++ ABC/449/C.rb
@@ -1,8 +1,11 @@
 n,l,r = gets.split.map(&:to_i)
-s = gets.chomp.chars
+s = gets.chomp
 
+h = Hash.new(0)
 ans = 0
-(n - l).times do |i|
-  ans += s[(l + i)..(r + i)].count(s[i])
+n.times do |j|
+  h[s[j - l]] += 1 if (j - l) >= 0
+  h[s[j - r - 1]] -= 1 if (j - r - 1) >= 0
+  ans += h[s[j]]
 end
 puts ans
