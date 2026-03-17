d97fbff7b950517a31334e9ba615fa53271b4c43
djkazunoko <djkazunoko@gmail.com>
2026-03-17 14:48:49 +0900

    ABC448-C (TLE) (#55)

--- ABC/448/C.rb
+++ ABC/448/C.rb
@@ -15,10 +15,5 @@ q.times do
     h2[balls[b_ary[j] - 1]] -= 1
   end
 
-  h2.sort.each do |ary|
-    if ary[1] != 0
-      puts ary[0]
-      break
-    end
-  end
+  puts h2.select { |k,v| v != 0 }.keys.min
 end
