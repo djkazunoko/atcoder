94c48cfe43871cd9d16d0b87bc9521fc01ea50dc
djkazunoko <djkazunoko@gmail.com>
2026-03-18 12:12:53 +0900

    ABC448-C インデックスで管理(AC) (#55)

--- ABC/448/C.rb
+++ ABC/448/C.rb
@@ -1,16 +1,15 @@
 n,q = gets.split.map(&:to_i)
 a = gets.split.map(&:to_i)
 
-a_6 = a.sort[..5]
+a_6 = a.map.with_index { |v, idx| [v, idx] }.sort[..5]
 q.times do
   k = gets.to_i
-  b = gets.split.map(&:to_i).map{a[_1 - 1]}.sort
-  b << 0
-  (k + 1).times do |i|
-    if a_6[i] != b[i]
-      puts a_6[i]
+  b = gets.split.map { _1.to_i - 1}.to_set
+
+  a_6.each do |v, idx|
+    unless b.include?(idx)
+      puts v
       break
     end
   end
 end
-
