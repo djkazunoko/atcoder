eef4b83754c9101731dfac7415f8d27ca755adf3
djkazunoko <djkazunoko@gmail.com>
2026-03-27 12:32:34 +0900

    ABC421-B OOP化 (AC) (#7)

--- ABC/421/B.rb
+++ ABC/421/B.rb
@@ -1,18 +1,24 @@
-x,y = gets.split
-
-def rev(str)
-  if str.length >= 2
-    str.reverse.to_i
-  else
-    str.to_i
+class Reverser
+  def self.call(x)
+    x.to_s.reverse.to_i
   end
 end
 
-arr = [x, y]
+class Sequence
+  def initialize(x, y)
+    @values = [x, y]
+  end
 
-(3..10).each do |i|
-  sx = (arr[i-2].to_i + arr[i-3].to_i).to_s
-  arr << rev(sx)
+  def generate(n)
+    (3..n).each do |i|
+      x = @values[-2] + @values[-1]
+      @values << Reverser.call(x)
+    end
+    @values
+  end
 end
 
-p arr[9]
+x,y = gets.split.map(&:to_i)
+sequence = Sequence.new(x, y)
+a = sequence.generate(10)
+puts a[-1]
