975252c4adcd82fadbc400c263e39f643327374b
djkazunoko <djkazunoko@gmail.com>
2026-04-10 10:52:30 +0900

    ABC452-C (TLE) (#59)

--- ABC/452/C.rb
+++ ABC/452/C.rb
@@ -3,22 +3,30 @@ ab_ary = n.times.map { gets.split.map(&:to_i) }
 m = gets.to_i
 s_ary = m.times.map { gets.chomp }
 
-s_by_size = Hash.new { |hash, key| hash[key] = [] }
+s_by_size = Array.new(10) { Array.new(_1 + 1) { '' } }
 m.times do |i|
-  s_by_size[s_ary[i].size] << s_ary[i]
+  s = s_ary[i]
+  (s.size).times do |j|
+    s_by_size[(s.size) - 1][j] << s[j]
+  end
+end
+
+t = []
+n.times do |i|
+  a,b = ab_ary[i]
+  t << s_by_size[a - 1][b - 1]
 end
 
 m.times do |j|
   s = s_ary[j]
-  if s.size < n
+  if s.size != n
     puts 'No'
     next
   end
 
   ok = true
   n.times do |i|
-    a,b = ab_ary[i]
-    if !s_by_size[a].map { _1[b - 1] }.include?(s[i])
+    if !t[i].include?(s[i])
       ok = false
       break
     end
