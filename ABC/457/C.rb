f5d436a675b4937583acca055b0a7da25e682dbb
djkazunoko <djkazunoko@gmail.com>
2026-05-10 13:18:47 +0900

    ABC457-C (RE) (#65)

--- ABC/457/C.rb
+++ ABC/457/C.rb
@@ -7,10 +7,31 @@ n,k = gsi
 a = n.times.map {gsi}
 c = gsi
 
-b = []
+# L * C の累積和
+l = []
+a.map {l << _1[0]}
 
+lc = l.zip(c).map {_1 * _2}
+s = [0]
 n.times do |i|
-  b << (a[i][1..] * c[i])
+  s << s[i] + lc[i]
 end
 
-puts b.flatten[k-1]
+# 累積和とKを比較して、対象のaを決める
+k_before_idx = s.bsearch_index { _1 > k } - 1
+k_before = s[k_before_idx]
+if k - k_before == 0
+  # Kが一致した時はtarget_aryの末尾が答え
+  puts a[k_before_idx - 1].last
+  exit
+else
+  target_ary = a[k_before_idx]
+end
+
+# target_aryの何番目か
+mod = (k - k_before) % target_ary[0]
+if mod == 0
+  puts target_ary.last
+else
+  puts target_ary[mod]
+end
