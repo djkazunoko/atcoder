8a66a8a8723aff0be14a1c702225ebd2a9dad55e
djkazunoko <djkazunoko@gmail.com>
2026-07-01 16:58:05 +0900

    ABC456-C (WA) (#64)

---
+++ ABC/456/C.rb
@@ -0,0 +1,20 @@
+def gi; gets.to_i; end
+def gc; gets.chomp; end
+def gsi; gets.split.map(&:to_i); end
+def pyn(x); puts(x ? 'Yes' : 'No'); end
+
+s = gc
+
+cnt = 0
+substr = ''
+s.each_char do |c|
+  if substr[-1] == c
+    cnt += (1..substr.size).sum
+    substr = c
+  else
+    substr << c
+  end
+end
+
+cnt += (1..substr.size).sum
+puts cnt
