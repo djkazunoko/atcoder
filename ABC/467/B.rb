63aa7500f09cd9ce937bf9ca2073612e655f9bf9
djkazunoko <djkazunoko@gmail.com>
2026-07-22 12:43:06 +0900

    ABC467-B 解説を見て修正(AC) (#73)

--- ABC/467/B.rb
+++ ABC/467/B.rb
@@ -4,20 +4,15 @@ def gsi; gets.split.map(&:to_i); end
 def pyn(x); puts(x ? 'Yes' : 'No'); end
 
 n = gi
-buy = n.times.map {gets.split}
-x,y = 10000,10000
-
-n.times do |i|
-  a,b,s = buy[i]
+ans = 0
+n.times do
+  a,b,s = gets.split
   a = a.to_i
   b = b.to_i
-  y -= a
 
   if s == 'keep'
-    x -= b
-  else
-    x -= a
+    ans += b - a
   end
 end
 
-puts y - x
+puts ans
