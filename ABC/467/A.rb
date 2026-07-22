b2bd0a127348de095e39c3791a9ab128c5752d64
djkazunoko <djkazunoko@gmail.com>
2026-07-22 12:34:31 +0900

    ABC467-A 解説を見て修正(AC) (#73)

--- ABC/467/A.rb
+++ ABC/467/A.rb
@@ -3,12 +3,6 @@ def gc; gets.chomp; end
 def gsi; gets.split.map(&:to_i); end
 def pyn(x); puts(x ? 'Yes' : 'No'); end
 
-require "bigdecimal"
-
 h,w = gsi
-h = BigDecimal(h)
-w = BigDecimal(w)
 
-bmi = w / (h / 100) / (h / 100)
-
-pyn(bmi >= 25)
+pyn(400 * w >= h * h)
