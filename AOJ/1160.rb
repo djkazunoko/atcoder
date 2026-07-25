9dd1b57ff30769bcc560d4d59501d46365f4f3cc
djkazunoko <djkazunoko@gmail.com>
2026-07-25 12:48:53 +0900

    AOJ1160 リファクタリング(解説AC)

--- AOJ/1160.rb
+++ AOJ/1160.rb
@@ -1,8 +1,4 @@
-def dfs(grid, visited, i, j, h, w)
-  visited[i][j] = true
-  return if grid[i][j] == 0
-
-  dir = [
+DIRECTIONS = [
     [1, 0],
     [0, 1],
     [-1, 0],
@@ -13,17 +9,21 @@ def dfs(grid, visited, i, j, h, w)
     [-1, 1]
   ]
 
-  dir.each do |di, dj|
+def dfs(grid, visited, i, j)
+  visited[i][j] = true
+
+  h = grid.size
+  w = grid[0].size
+
+  DIRECTIONS.each do |di, dj|
     next_i = i + di
     next_j = j + dj
 
     next if next_i < 0 || next_i >= h || next_j < 0 || next_j >= w
     next if grid[next_i][next_j] == 0
     next if visited[next_i][next_j]
-    dfs(grid, visited, next_i, next_j, h, w)
+    dfs(grid, visited, next_i, next_j)
   end
-
-  $riku += 1
 end
 
 loop do
@@ -31,17 +31,15 @@ loop do
   break if w == 0 && h == 0
   grid = h.times.map {gets.split.map(&:to_i)}
   visited = Array.new(h) { Array.new(w, false) }
-  $shima = 0
 
+  shima = 0
   h.times do |i|
     w.times do |j|
-      if visited[i][j] == false
-        $riku = 0
-        dfs(grid, visited, i, j, h, w)
-        $shima += 1 if $riku >= 1
-      end
+      next if grid[i][j] == 0
+      next if visited[i][j]
+      dfs(grid, visited, i, j)
+      shima += 1
     end
   end
-
-  puts $shima
+  puts shima
 end
