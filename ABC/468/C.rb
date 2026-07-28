32dd3bdab331190203f5c2d746a51d2806fc3698
djkazunoko <djkazunoko@gmail.com>
2026-07-28 23:20:11 +0900

    ABC468-C (解説TLE) (#74)

---
+++ ABC/468/C.rb
@@ -0,0 +1,16 @@
+def gi; gets.to_i; end
+def gc; gets.chomp; end
+def gsi; gets.split.map(&:to_i); end
+def pyn(x); puts(x ? 'Yes' : 'No'); end
+
+n = gi
+p = gsi
+q = gsi
+
+ans = 0
+(1..n).to_a.permutation.to_a.each do |ary|
+  if (ary <=> p).positive? && (ary <=> q).negative?
+    ans += 1
+  end
+end
+puts ans
