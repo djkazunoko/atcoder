aa8a423d878ce9088f64cf75e021528d16b2008a
djkazunoko <djkazunoko@gmail.com>
2026-07-29 15:06:34 +0900

    ABC126-D リファクタリング(解説AC)

--- ABC/126/D.rb
+++ ABC/126/D.rb
@@ -1,25 +1,22 @@
+def dfs(v, graph, color)
+  graph[v].each do |to, w|
+    next if color[to] != -1
+    color[to] = w.even? ? color[v] : 1 - color[v]
+    dfs(to, graph, color)
+  end
+end
+
 n = gets.to_i
 
 graph = Array.new(n) {[]}
 
 (n-1).times do
   u, v, w = gets.split.map(&:to_i)
-  graph[u - 1] << [v - 1, w]
-  graph[v - 1] << [u - 1, w]
-end
-
-def dfs(v, graph, color)
-  graph[v].each do |ary|
-    to, w = ary
-    next if color[to] != -1
-    if w.even?
-      color[to] = color[v]
-    else
-      color[to] = color[v] == 0 ? 1 : 0
-    end
-
-    dfs(to, graph, color)
-  end
+  u -= 1
+  v -= 1
+  
+  graph[u] << [v, w]
+  graph[v] << [u, w]
 end
 
 color = Array.new(n, -1)
