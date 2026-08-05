bc2e4261b14bf8fff19d1744c897ed3f139a8653
djkazunoko <djkazunoko@gmail.com>
2026-08-05 23:47:46 +0900

    ABC450-C リファクタリング(解説AC) (#57)

--- ABC/450/C.rb
+++ ABC/450/C.rb
@@ -3,11 +3,6 @@ def gc; gets.chomp; end
 def gsi; gets.split.map(&:to_i); end
 def pyn(x); puts(x ? 'Yes' : 'No'); end
 
-h,w = gsi
-grid = Array.new(h) {gets.chomp.chars}
-visited = Array.new(h) {Array.new(w,false)}
-cc = []
-
 DIRECTIONS = [
   [1, 0],
   [0, 1],
@@ -15,12 +10,12 @@ DIRECTIONS = [
   [0, -1]
 ]
 
-def dfs(grid, visited, i, j, cc)
-  visited[i][j] = true
+def dfs(grid, visited, i, j)
   h = grid.size
   w = grid[0].size
   
-  cc << [i,j]
+  visited[i][j] = true
+  enclosed = (0 < i && i < h-1 && 0 < j && j < w-1)
   
   DIRECTIONS.each do |di, dj|
     next_i = i + di
@@ -30,26 +25,23 @@ def dfs(grid, visited, i, j, cc)
     next if visited[next_i][next_j]
     next if grid[next_i][next_j] == '#'
 
-    dfs(grid, visited, next_i, next_j, cc)
+    enclosed &= dfs(grid, visited, next_i, next_j)
   end
+
+  enclosed
 end
+
+h,w = gsi
+grid = Array.new(h) {gets.chomp.chars}
+visited = Array.new(h) {Array.new(w,false)}
 
 cnt = 0
 h.times do |i|
   w.times do |j|
-    if grid[i][j] == '.' && visited[i][j] == false
-      dfs(grid, visited, i, j, cc)
+    next if grid[i][j] == '#'
+    next if visited[i][j]
 
-      ok = true
-      cc.each do |i,j|
-        if i == 0 || i == h - 1 || j == 0 || j == w - 1
-          ok = false
-          break
-        end
-      end
-      cnt += 1 if ok
-      cc.clear
-    end
+    cnt += 1 if dfs(grid, visited, i, j)
   end
 end
 puts cnt
