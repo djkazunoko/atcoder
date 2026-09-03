fed0b0a747c5c8f818948e4b388eb071985e9f69
djkazunoko <djkazunoko@gmail.com>
2026-09-03 21:49:20 +0900

    ABC459-A (自力AC) (#67)

---
+++ ABC/459/A.rb
@@ -0,0 +1,12 @@
+def gi; gets.to_i; end
+def gc; gets.chomp; end
+def gsi; gets.split.map(&:to_i); end
+def pyn(x); puts(x ? 'Yes' : 'No'); end
+
+x = gi
+
+str = 'HelloWorld'
+
+str.slice!(x-1)
+
+puts str
